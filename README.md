c04c35c83401c5b3539ac04199c6e8df9eb6c2c0
Eduardo Diaz <ssh.eduardo@gmail.com>
2012-01-06 22:57:16 +0100

    primera version

---
+++ README.md
@@ -0,0 +1,56 @@
+Historia
+--------
+Esta clase nace porque no te encontraba una clase de pasarela de pagos (TPV) que se pueda integrar directamente en una web, existen
+muchas pero para varios CMS y no me servian, solo quería montar algo fácil que pueda usar, los ejemplos que vienen en la documentación oficial era muy simple así que decidi realizar esta clase y ahora lo comparto con todos.
+
+Introducción
+------------
+La clase sermepa sirve para generar el formulario que se comunicará con la pasarela de pagos que usan muchos bancos como: Sabadell, Lacaixa, etc.
+
+Es una versión que ira creciendo y actualizandose poco a poco y mejorandolo.
+Si lo usas en algún proyecto y te ayudo en algo no dudas en decirmelo
+
+Requerimientos
+--------------
+PHP 5
+
+Creditos
+--------
+	Clase creada por Eduardo Diaz, Madrid 2012
+	Twitter: @eduardo_dx
+
+Como usar la clase
+------------------
+
+Ejemplo:
+Primero asignamos los parámetros
+
+	$pasarela = new Sermepa();
+    $pasarela->importe(10.50);
+    $pasarela->pedido(date('ymdHis'));	//generamos el número de recibo usando date por ejemplo
+    $pasarela->clave('xxxxxxx');	//clave asignada por el banco.
+    $pasarela->codigofuc('xxxxxxx');
+    $pasarela->producto_descripcion('Demo');
+    $pasarela->titular('Usuario');
+    $pasarela->nombre_comercio('Ejemplo');
+    //Si el comercio tiene notificación "on-line". URL del comercio que recibirá un post con los datos de la transacción .
+    $pasarela->url_notificacion('http://www.example.com/notificacion.php');	
+    $pasarela->url_ok('http://www.example.com/ok.php');	// Si le das aceptar finalizada la compra desde la pasarela de pagos
+    $pasarela->url_ko('http://www.example.com/ko.php'); // Si le das cancelar desde la pasarela de pagos
+    $pasarela->firma();
+
+Generamos el formulario
+
+	$formulario = $pasarela->create_form();
+
+Con esto generamos el form para la comunicación con la pasarela de pagos.
+Solo queda agregar un input submit personalizado o por medio de javascript realizar el submit.
+
+Nota:
+	Por defecto se conecta por la pasarela de pruebas para cambiar a un entorno real usar el método: set_entorno('real')
+
+
+TODO:
+	- Agregar nombre dinámico del formulario.
+	- Input submit que actualmente no lo genero.
+	
---
+++ sermepa.php
@@ -0,0 +1,301 @@
+<?php
+
+class Sermepa{
+    private $_setEntorno;
+    private $_setImporte;
+    private $_setMoneda;
+    private $_setPedido;
+    private $_setProductoDescripcion;
+    private $_setTitular;
+    private $_setFuc;
+    private $_setTerminal;
+    private $_setTransactionType;
+    private $_setUrlNotificacion;
+    private $_setClave;
+    private $_setUrlOk;
+    private $_setUrlKo;
+    private $_setFirma;
+    private $_setNombreComercio;
+    private $_setIdioma;
+    private $_setMethods;
+    
+    public function __construct(){
+        
+        $this->_setEntorno='https://sis-t.sermepa.es:25443/sis/realizarPago';
+        $this->_setMoneda ='978';
+        $this->_setTerminal =1;
+        $this->_setTransactionType=0;
+        $this->_setIdioma = '001';
+        $this->_setMethods='T';
+    }
+    
+    #-#####################
+    # desc: Asignamos el idioma por defecto 001 = Español
+    # param: codeidioma: codigo de idioma [Castellano-001, Inglés-002, Catalán-003, Francés-004, Alemán-005,
+    #                   Holandés-006, Italiano-007, Sueco-008, Portugués-009, Valenciano-010, Polaco-011, Gallego-012 y Euskera-013.]
+    public function set_idioma($codeidioma){
+        $this->_setIdioma = $codeidioma;
+    }#set_idioma()
+    
+    #-######################
+    #desc: Asignamos que tipo de entorno vamos a usar si Pruebas o Real (por defecto esta en pruebas)
+    #param: entorno (pruebas, real)
+    public function set_entorno($entorno='pruebas'){
+        if(trim($entorno) == 'real'){
+            //real
+            $this->_setEntorno='https://sis.sermepa.es/sis/realizarPago';
+        }
+        else{
+            //pruebas
+            $this->_setEntorno ='https://sis-t.sermepa.es:25443/sis/realizarPago';
+        }            
+    }#-#set_entorno()
+
+    #-######################
+    #desc: Retornamos la URL que va en el form para los pagos ya sea en pruebas o real
+    #return: URL sermepa (real o pruebas)    
+    public function geturlentorno(){
+        return $this->_setEntorno;    
+    }#-#geturlentorno()
+    
+    #-######################
+    #desc: Asignamos el importe de la compra
+    #param: importe: total de la compra a pagar
+    #return: Retornamos el importe ya modificado
+    public function importe($importe=0){
+        $importe = $this->parseFloat($importe);
+        
+        // sermepa nos dice Para Euros las dos últimas posiciones se consideran decimales.
+        $importe = intval($importe*100);
+        $this->_setImporte=$importe;
+        return $importe;
+    }#-#importe()
+    
+    
+    #-##############################
+    #desc: Asignamos el tipo de moneda
+    #param: moneda: 978 para Euros, 840 para Dólares, 826 para libras esterlinas y 392 para Yenes.
+    public function moneda($moneda='978'){
+        if($moneda == '978' || $moneda =='840' || $moneda =='826' || $moneda =='392' ){
+            $this->_setMoneda = $moneda;
+        }
+        else{
+            throw new Exception('Moneda no valida');
+        }
+        
+    }#-#moneda()
+    
+    #-##############################
+    #desc: Asignamos el número de pedido a nuestra compra (Los 4 primeros dígitos deben ser numéricos)
+    #param: pedido: Numero de pedido alfanumérico
+    public function pedido($pedido=''){
+        if(strlen(trim($pedido))> 0){
+            $this->_setPedido = $pedido;
+        }
+        else{
+            throw new Exception('Falta agregar el número de pedido');
+        }
+        
+    }#-#pedido()
+    
+    #-##############################
+    #desc: Asigmanos la descripción del producto (Obligatorio)
+    #param: producto: Descripción del producto
+    public function producto_descripcion($producto=''){
+        if(strlen(trim($producto)) > 0){
+            //asignamos el producto
+            $this->_setProductoDescripcion = $producto;
+        }
+        else{
+            throw new Exception('Falta agregar la descripción del producto');
+        }
+    }#-#producto_descripcion()
+    
+    #-###############################
+    #desc: Asigmanos el nombre del usuario que realiza la compra (Obligatorio)
+    #param: titular: Nombre del usuario (por ejemplo Juan Perez)
+    public function titular($titular=''){
+        if(strlen(trim($titular)) > 0){
+            $this->_setTitular = $titular;
+        }
+        else{
+            throw new Exception('Falta agregar el titular que realiza la compra');
+        }
+    }#-#titular()
+    
+    #-###########################
+    #desc: Asignamos el código FUC del comercio (Obligatorio)
+    #param: fuc: Código fuc que nos envía sermepa
+    public function codigofuc($fuc=''){
+        if(strlen(trim($fuc)) > 0){
+            $this->_setFuc = $fuc;
+        }
+        else{
+            throw new Exception('Falta agregar el código FUC');
+        }  
+    }#-#codigofuc
+    
+    #-###########################
+    #desc: Asignar el tipo de terminal por defecto 1 en Sadabell (Obligatorio)
+    #param: terminal: Número de terminal que le asignará su banco.
+    public function terminal($terminal=1){
+        if(intval($terminal) !=0){
+            $this->_setTerminal = $terminal;
+        }
+        else{
+            throw new Exception('El terminal no es valido');
+        }
+    }#-#terminal()
+    
+    #-##############################
+    #desc: Asignar el tipo de transacción por defecto 0 que es autorización (Obligatorio)
+    #param: transactiontype: tipo de transacción
+    public function transaction_type($transactiontype=0){
+        if(strlen(trim($transactiontype)) > 0){
+            $this->_setTransactionType= $transactiontype;
+        }
+        else{
+            throw new Exception('Falta agregar el tipo de transacción');
+        }
+    }#-#transaction_type()
+    
+    #-################################
+    # desc: Nombre del comercio que será reflejado en el ticket del comercio (Opcional)
+    # param: nombrecomercio = Nombre del comercio
+    public function nombre_comercio($nombrecomercio=''){
+        $nombrecomercio = trim($nombrecomercio);
+        $this->_setNombreComercio = $nombrecomercio;
+    }#-#nombre_comercio()
+    
+    #-################################
+    #desc: Si el comercio tiene notificación "on-line". URL del comercio que recibirá un post con los datos de la transacción (Obligatorio).
+    #param: url_notificacion: Url para guardar los datos recibidos por el comercio.
+    public function url_notificacion($url_notificacion=''){
+        if(strlen(trim($url_notificacion)) > 0){
+            $this->_setUrlNotificacion = $url_notificacion;
+        }
+        else{
+            throw new Exception('Falta agregar url de notificacion');
+        }
+    }#-#url_notificacion()
+    
+    #-################################
+    #desc: Si se envía será utilizado como URLOK ignorando el configurado en el módulo de administración en caso de tenerlo (Opcional).
+    #param: url: Url donde mostrar un mensaje si se realizo correctamente el pago
+    public function url_ok($url=''){
+        $this->_setUrlOk = $url;
+    }#-#url_ok()
+    
+    #-################################
+    #desc: Si se envía será utilizado como URLKO ignorando el configurado en el módulo de administración en caso de tenerlo (Opcional).
+    #param: url: Url donde mostrar un mensaje si se produjo un problema al realizar el pago
+    public function url_ko($url=''){
+        $this->_setUrlKo = $url;
+    }#-#url_ko()
+    
+    #-###############################
+    #desc: Clave proporcionada por el banco (Obligatorio)
+    #param: clave: Clave asignada
+    public function clave($clave=''){
+        if(strlen(trim($clave)) > 0){
+            $this->_setClave = $clave;
+        }
+        else{
+            throw new Exception('Falta agregar la clave proporcionada por sermepa');
+        }
+    }#-#clave()
+
+    #-################################
+    # desc: Tipo de pago [T = Pago con Tarjeta, R = Pago por Transferencia, D = Domiciliacion]
+    #       por defecto es T
+    public function methods($metodo='T'){
+        $this->_setMethods= $metodo;
+    }#-#methods()
+    
+        
+    #-################################
+    #desc: Firma que se le envía a sermepa (Obligatorio)    
+    public function firma(){
+                
+        $mensaje = $this->_setImporte . $this->_setPedido . $this->_setFuc . $this->_setMoneda . $this->_setTransactionType . $this->_setUrlNotificacion . $this->_setClave;
+        if(strlen(trim($mensaje)) > 0){
+            // Cálculo del SHA1		    		
+    		$this->_setFirma = strtoupper(sha1($mensaje));	   
+        }
+        else{
+            throw new Exception('Falta agregar la firma, Obligatorio');
+        }
+    }#-#firma()
+    
+    #-##########################
+    #des: Generamos el form a incluir en nuestro html
+    public function create_form(){
+        $formulario='
+        <form action="'.$this->_setEntorno.'" method="post" id="servired_form" name="servired_form" >
+            <input type="hidden" name="Ds_Merchant_Amount" value="'.$this->_setImporte.'" />
+            <input type="hidden" name="Ds_Merchant_Currency" value="'.$this->_setMoneda.'" />
+            <input type="hidden" name="Ds_Merchant_Order" value="'.$this->_setPedido.'" />
+            <input type="hidden" name="Ds_Merchant_MerchantCode" value="'.$this->_setFuc.'" />
+            <input type="hidden" name="Ds_Merchant_Terminal" value="'.$this->_setTerminal.'" />
+            <input type="hidden" name="Ds_Merchant_TransactionType" value="'.$this->_setTransactionType.'" />
+            <input type="hidden" name="Ds_Merchant_Titular" value="'.$this->_setTitular.'" />
+            <input type="hidden" name="Ds_Merchant_MerchantName" value="'.$this->_setNombreComercio.'" />
+            <input type="hidden" name="Ds_Merchant_MerchantURL" value="'.$this->_setUrlNotificacion.'" />
+            <input type="hidden" name="Ds_Merchant_ProductDescription" value="'.$this->_setProductoDescripcion.'" />
+            <input type="hidden" name="Ds_Merchant_ConsumerLanguage " value="'.$this->_setIdioma.'" />
+            <input type="hidden" name="Ds_Merchant_UrlOK" value="'.$this->_setUrlOk.'" />
+            <input type="hidden" name="Ds_Merchant_UrlKO" value="'.$this->_setUrlKo.'" />
+            <input type="hidden" name="Ds_Merchant_PayMethods" value="'.$this->_setMethods.'" />
+            <input type="hidden" name="Ds_Merchant_MerchantSignature" value="'.$this->_setFirma.'" />        
+        </form>        
+        ';
+        return $formulario;
+    }#-#create_form()
+    
+    private function parseFloat($ptString)
+    {
+    	if (strlen($ptString) == 0) {
+    		return false;
+    	}
+    	$pString = str_replace(" ", "", $ptString);
+    	if (substr_count($pString, ",") > 1)
+    	$pString = str_replace(",", "", $pString);
+    	if (substr_count($pString, ".") > 1)
+    	$pString = str_replace(".", "", $pString);
+    	$pregResult = array();
+    	$commaset = strpos($pString,',');
+    	if ($commaset === false) {
+    		$commaset = -1;
+    	}
+    	$pointset = strpos($pString,'.');
+    	if ($pointset === false) {
+    		$pointset = -1;
+    	}
+    	$pregResultA = array();
+    	$pregResultB = array();
+    	if ($pointset < $commaset) {
+    		preg_match('#(([-]?[0-9]+(\.[0-9])?)+(,[0-9]+)?)#', $pString, $pregResultA);
+    	}
+    	preg_match('#(([-]?[0-9]+(,[0-9])?)+(\.[0-9]+)?)#', $pString, $pregResultB);
+    	if ((isset($pregResultA[0]) && (!isset($pregResultB[0])
+    	|| strstr($preResultA[0],$pregResultB[0]) == 0
+    	|| !$pointset))) {
+    		$numberString = $pregResultA[0];
+    		$numberString = str_replace('.','',$numberString);
+    		$numberString = str_replace(',','.',$numberString);
+    	}
+    	elseif (isset($pregResultB[0]) && (!isset($pregResultA[0])
+    	|| strstr($pregResultB[0],$preResultA[0]) == 0
+    	|| !$commaset)) {
+    		$numberString = $pregResultB[0];
+    		$numberString = str_replace(',','',$numberString);
+    	}
+    	else {
+    		return false;
+    	}
+    	$result = (float)$numberString;
+    	return $result;
+    }    
+}
+
+?>
