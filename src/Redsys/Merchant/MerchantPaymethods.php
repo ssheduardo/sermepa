c6309a2bd181b3df38ba92c56c812bb9f54bd153
Fernando Herrero <ininflamable@gmail.com>
2023-12-15 14:36:06 +0100

    Primera propuesta

---
+++ src/Redsys/Merchant/MerchantPaymethods.php
@@ -0,0 +1,15 @@
+<?php
+  declare(strict_types=1);
+
+	namespace Redsys\Merchant;
+
+	class MerchantPaymethods {
+		public const ALL = '';
+		public const TARJETA = 'C';
+		public const TARJETA_IUPAY = 'T';
+		public const BIZUM = 'z';
+		public const PAYPAL = 'P';
+		public const TRANSFERENCIA = 'R';
+		public const MASTERPASS = 'N';
+		public const IUPAY = 'O';
+	}
---
+++ src/Redsys/Merchant/MerchantTransactionTypes.php
@@ -0,0 +1,17 @@
+<?php
+  declare(strict_types=1);
+
+	namespace Redsys\Merchant;
+
+	class MerchantTransactionTypes {
+		public const AUTORIZACION = 0; // Autorización
+		public const PREAUTORIZACION = 1; // Preautorización
+		public const CONFIRMACION_PREAUTORIZACION = 2; // Confirmación de preautorización
+		public const DEVOLUCION_AUTOMATICA = 3; // Devolución Automática
+		public const DEVOLUCION_SIN_ORIGINAL = 'Y'; // Devolución sin original
+		public const AUTENTICACION = 7; // Autenticación
+		public const CONFIRMACION_AUTENTICACION = 8; // Confirmación de Autenticación
+		public const ANULACION_AUTORIZACION = 9; // Anulación de Preautorización o Autorización
+		public const ABONO_APUESTAS = 37; // Abono de Apuestas
+		public const PAYGOLD = 'F'; // Paygold
+	}
